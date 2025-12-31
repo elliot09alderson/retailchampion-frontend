ef76dc46523447eb1a0212089e16c01e559b8159
pratik verma <erpratik@pratiks-MacBook-Air.local>
2025-12-31 11:00:44 +0530

    adsad

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 // API Configuration
-// export const API_BASE_URL = 'https://retailchampions.com';
-export const API_BASE_URL = 'http://localhost:5007';
+export const API_BASE_URL = 'https://retailchampions.com';
+// export const API_BASE_URL = 'http://localhost:5007';
 
 // API Endpoints
 export const API_ENDPOINTS = {
